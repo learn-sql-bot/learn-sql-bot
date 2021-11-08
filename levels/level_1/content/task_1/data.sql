aa326d9885ea36742313d83ce01195574e7ee5db
Gleb Kushedow <kushedow@gmail.com>
2021-11-09 00:15:54 +0300

    Update data.sql

--- levels/level_1/content/task_1/data.sql
+++ levels/level_1/content/task_1/data.sql
@@ -9,15 +9,14 @@
 --
 
 CREATE TABLE IF NOT EXISTS `user_details` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
   `username` varchar(255) DEFAULT NULL,
   `first_name` varchar(50) DEFAULT NULL,
   `last_name` varchar(50) DEFAULT NULL,
   `gender` varchar(10) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
-  `status` tinyint(10) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) AUTO_INCREMENT=1 ;
+  `status` tinyint(10) DEFAULT NULL
+) 
 
 --
 -- Dumping data for table `user_details`
